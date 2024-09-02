7cbc3d733c6d1b81c0e02e08cbe739823de21dc4
Alex Monge <monge.alex572@gmail.com>
2024-09-02 18:16:18 -0500

    fixed counter

--- src/components/VisitorCounter.js
+++ src/components/VisitorCounter.js
@@ -1,29 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const VisitorCounter = () => {
-  const [visitorCount, setVisitorCount] = useState(0);
-  
+  const [visitorCount, setVisitorCount] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    // Replace with your actual API endpoint
     const apiUrl = 'https://waqme3i9e4.execute-api.us-east-1.amazonaws.com/prod/incrementCounter';
 
     const fetchVisitorCount = async () => {
       try {
-        const response = await axios.get(apiUrl);
-        setVisitorCount(response.data.visitor_count);  // Update with your actual response format
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        // Parse the response as JSON
+        const data = await response.json();
+        console.log('API response:', data); // Debugging line
+
+        // Parse the 'body' field, which is a string
+        const body = JSON.parse(data.body);
+
+        if (body.visitor_count !== undefined) {
+          setVisitorCount(body.visitor_count);
+        } else {
+          throw new Error('Visitor count not found in response');
+        }
       } catch (error) {
         console.error('Error fetching visitor count:', error);
+        setError('Error fetching visitor count');
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchVisitorCount();
-  }, []);  // Empty dependency array means this effect runs once when the component mounts
+  }, []); // Empty dependency array means this effect runs once when the component mounts
 
   return (
     <div>
       <h1>Visitor Counter</h1>
-      <p>Current Visitor Count: {visitorCount}</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <p>Current Visitor Count: {visitorCount}</p>
+      )}
     </div>
   );
 };
